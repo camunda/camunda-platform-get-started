f4f728543f2fd65135b193a14c498106c637ff29
Bernd Ruecker <bernd.ruecker@camunda.com>
2021-07-15 14:36:54 +0200

    adjusted to non-blocking code in Spring and added hint to readme

--- java/src/main/java/io/camunda/getstarted/EmailWorker.java
+++ java/src/main/java/io/camunda/getstarted/EmailWorker.java
@@ -18,7 +18,7 @@ public class EmailWorker {
 
         jobClient.newCompleteCommand(job.getKey()).send()
           // join(); <-- This would block for the result. While this is easier-to-read code, it has limitations for parallel work.
-          // So the following code leverages reactive programming instead. This is discssed in https://blog.bernd-ruecker.com/writing-good-workers-for-camunda-cloud-61d322cad862
+          // Hence, the following code leverages reactive programming. This is discssed in https://blog.bernd-ruecker.com/writing-good-workers-for-camunda-cloud-61d322cad862.
           .whenComplete((result, exception) -> {
             if (exception == null) {
               LOG.info("Completed job successful with result:" + result);
--- spring/src/main/java/io/camunda/getstarted/EmailWorker.java
+++ spring/src/main/java/io/camunda/getstarted/EmailWorker.java
@@ -20,7 +20,16 @@ public class EmailWorker {
 
     LOG.info("Sending email with message content: {}", message_content);
 
-    client.newCompleteCommand(job.getKey()).send().join();
+    client.newCompleteCommand(job.getKey()).send()
+      // join(); <-- This would block for the result. While this is easier-to-read code, it has limitations for parallel work.
+      // Hence, the following code leverages reactive programming. This is discssed in https://blog.bernd-ruecker.com/writing-good-workers-for-camunda-cloud-61d322cad862.
+      .whenComplete((result, exception) -> {
+        if (exception == null) {
+          LOG.info("Completed job successful with result:" + result);
+        } else {
+          LOG.error("Failed to complete job", exception);
+        }
+      });    
   }
 
 }
