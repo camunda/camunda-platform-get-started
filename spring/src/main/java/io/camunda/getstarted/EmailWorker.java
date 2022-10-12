d83804e31ae0286ed10cbfb1b3bd0b78e359dcdc
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-10-12 15:55:57 +0200

    Updated to Spring Zeebe 8.1.0 (#150)

--- spring/src/main/java/io/camunda/getstarted/EmailWorker.java
+++ spring/src/main/java/io/camunda/getstarted/EmailWorker.java
@@ -1,7 +1,7 @@
 package io.camunda.getstarted;
 
 import io.camunda.zeebe.client.api.response.ActivatedJob;
-import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,7 +11,7 @@ public class EmailWorker {
 
   private final static Logger LOG = LoggerFactory.getLogger(EmailWorker.class);
 
-  @ZeebeWorker(type = "email", autoComplete = true)
+  @JobWorker(type = "email")
   public void sendEmail(final ActivatedJob job) {
     final String message_content = (String) job.getVariablesAsMap().get("message_content");
     LOG.info("Sending email with message content: {}", message_content);
