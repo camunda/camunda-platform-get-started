3103a6f1b91e973019e4d5dea9521e8ecba6d485
Sebastian Menski <sebastian.menski@camunda.com>
2022-12-05 10:27:06 +0100

    add workflow for java, spring, go, nodejs and csharp (#174)

--- java/src/main/java/io/camunda/getstarted/ZeebeClientFactory.java
+++ java/src/main/java/io/camunda/getstarted/ZeebeClientFactory.java
@@ -6,9 +6,9 @@ public class ZeebeClientFactory {
 
   public static ZeebeClient getZeebeClient() {
     return ZeebeClient.newCloudClientBuilder()
-        .withClusterId("365eed98-16c1-4096-bb57-eb8828ed131e")
-        .withClientId("GZVO3ALYy~qCcD3MYq~sf0GIszNzLE_z")
-        .withClientSecret(".RPbZc6q0d6uzRbB4LW.B8lCpsxbBEpmBX0AHQGzINf3.KK9RkzZW1aDaZ-7WYNJ")
+        .withClusterId("YOUR_CLUSTER_ID")
+        .withClientId("YOUR_CLIENT_ID")
+        .withClientSecret("YOUR_CLIENT_SECRET")
         .withRegion("bru-2")
         .build();
   }
